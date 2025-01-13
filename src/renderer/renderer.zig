af4c10c01e60c541b064c81fdd1e47acc51223cd
Dan Liebault <d.liebault@systancia.com>
2025-01-13 18:57:26 +0100

    feat: implement frame management with synchronization for rendering

--- src/renderer/renderer.zig
+++ src/renderer/renderer.zig
@@ -18,15 +18,36 @@ const renderer = struct {
     usingnamespace @import("syncs.zig");
 };
 
+const frame_t = struct {
+    render_fence: c.VkFence = undefined,
+    render_finished_sem: c.VkSemaphore = undefined,
+    image_available_sem: c.VkSemaphore = undefined,
+
+    pub fn init(self: *frame_t, device: c.VkDevice) !void {
+        self.render_fence = try renderer.create_fence(device);
+        self.render_finished_sem = try renderer.create_semaphore(device);
+        self.image_available_sem = try renderer.create_semaphore(device);
+    }
+
+    pub fn deinit(self: *frame_t, device: c.VkDevice) void {
+        c.vkDestroySemaphore(device, self.image_available_sem, null);
+        self.image_available_sem = undefined;
+
+        c.vkDestroySemaphore(device, self.render_finished_sem, null);
+        self.render_finished_sem = undefined;
+
+        c.vkDestroyFence(device, self.render_fence, null);
+        self.image_available_sem = undefined;
+    }
+};
+
 pub const renderer_t = struct {
     app: app_t = undefined,
     swapchain: swapchain_t = undefined,
     command_pool: c.VkCommandPool = undefined,
     command_buffers: []c.VkCommandBuffer = undefined,
-    render_fence: c.VkFence = undefined,
-    render_finished_sem: c.VkSemaphore = undefined,
-    image_available_sem: c.VkSemaphore = undefined,
-
+    frames: [3]frame_t = undefined,
+    current_frame: u8 = 0,
 
     pub fn init(self: *renderer_t, window: ?*sdl.SDL_Window) !void {
         self.app.instance = try renderer.init_instance();
@@ -54,17 +75,17 @@ pub const renderer_t = struct {
         self.command_pool = try renderer.create_command_pool(self.app.device, self.app.queues.queue_family_indices.graphics_family);
         self.command_buffers = try renderer.create_command_buffer(3, self.app.device, self.command_pool);
 
-        self.render_fence = try renderer.create_fence(self.app.device);
-        self.render_finished_sem = try renderer.create_semaphore(self.app.device);
-        self.image_available_sem = try renderer.create_semaphore(self.app.device);
+        for (&self.frames) |*frame| {
+            try frame.init(self.app.device);
+        }
     }
 
     pub fn deinit(self: *renderer_t) void {
         self.clean_swapchain();
 
-        c.vkDestroySemaphore(self.app.device, self.image_available_sem, null);
-        c.vkDestroySemaphore(self.app.device, self.render_finished_sem, null);
-        c.vkDestroyFence(self.app.device, self.render_fence, null);
+        for (&self.frames) |*frame| {
+            frame.deinit(self.app.device);
+        }
 
         c.vkDestroyDevice(self.app.device, null);
         c.vkDestroySurfaceKHR(self.app.instance, self.app.surface, null);
@@ -89,7 +110,22 @@ pub const renderer_t = struct {
         c.vkDestroySwapchainKHR(self.app.device, self.swapchain.handle, null);
     }
 
-    pub fn draw() void {
+    pub fn draw(self: *const renderer_t) void {
+        c.vkWaitForFences(self.app.device, 1, &self.frames[self.current_frame].render_fence, true, 1000);
+        c.vkResetFences(self.app.device, 1, &self.frames[self.current_frame].render_fence);
 
+        var image_index: u32 = 0;
+        _ = c.vkAcquireNextImageKHR(self.app.device, self.swapchain.handle, 1000, &self.frames[self.current_frame].image_available_sem, null, &image_index);
+
+
+        const wait_sem = [1]c.VkSemaphore{ self.frames[self.current_frame].image_available_sem };
+        const submit_info = c.VkSubmitInfo {
+            .sType = c.VK_STRUCTURE_TYPE_SUBMIT_INFO,
+            .waitSemaphoreCount = 1,
+            .pWaitSemaphores = wait_sem,
+            .pWaitDstStageMask = [1]c_int{ c.VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT },
+        };
+
+        _ = c.vkQueueSubmit(self.app.queues.graphics_queue, 1, &submit_info, self.frames[self.current_frame].render_fence);
     }
 };
