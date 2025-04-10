f9118155e5d057e43d541d24f70b0d66d421f9bd
Dan Liebault <dan.liebault@viacesi.fr>
2025-04-10 16:16:23 +0200

    fix: update window dimensions to 1280x960 in main function

--- src/main.zig
+++ src/main.zig
@@ -11,7 +11,10 @@ pub fn main() !u8 {
     }
     defer c.SDL_Quit();
 
-    const window = c.SDL_CreateWindow("Hello World", 800, 600, c.SDL_WINDOW_VULKAN);
+    const width = 1280;
+    const heigh = 960;
+
+    const window = c.SDL_CreateWindow("Hello World", width, heigh, c.SDL_WINDOW_VULKAN);
     if (window == null) {
         c.SDL_LogError(c.SDL_LOG_CATEGORY_APPLICATION, "Unable to create window: %s", c.SDL_GetError());
         return 1;
@@ -19,7 +22,7 @@ pub fn main() !u8 {
     defer c.SDL_DestroyWindow(window);
 
     // var engine = vk_engine{};
-    engine.init(window, 800, 600) catch {
+    engine.init(window, width, heigh) catch {
         c.SDL_LogError(c.SDL_LOG_CATEGORY_APPLICATION, "Unable to initialize Vulkan engine");   
         return 1;
     };
