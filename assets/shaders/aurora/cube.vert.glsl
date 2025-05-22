9ba3fac9369b5a44fc7d3237e011dfeea657aa02
Dan Liebault <dan.liebault@viacesi.fr>
2025-05-22 20:46:52 +0200

    refactor: optimize variable declarations and improve readability in shader code

--- assets/shaders/aurora/cube.vert.glsl
+++ assets/shaders/aurora/cube.vert.glsl
@@ -2,13 +2,7 @@
 
 #extension GL_EXT_buffer_reference : require
 
-struct vertex_t {
-    vec3 position;
-	float uv_x;
-	vec3 normal;
-	float uv_y;
-	vec4 color;
-};
+#include "types.glsl"
 
 layout(set = 0, binding = 0) uniform  SceneData{   
 	mat4 view;
--- assets/shaders/aurora/world.comp.glsl
+++ assets/shaders/aurora/world.comp.glsl
@@ -29,25 +29,22 @@ layout(std430, binding = 3) buffer ChunkData {
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 8) in;
 
 void main() {
-    uint x = gl_GlobalInvocationID.x;
-    uint y = gl_GlobalInvocationID.y;
-    uint z = gl_GlobalInvocationID.z;
-
-    uint index = x + (y * CHUNK_SIZE) + (z * CHUNK_SIZE_SQR);
-    if (voxels[index].data.y == 0) // AIR
-    {
+    const uint x = gl_GlobalInvocationID.x;
+    const uint y = gl_GlobalInvocationID.y;
+    const uint z = gl_GlobalInvocationID.z;
+ 
+    const uint index = x + (y * CHUNK_SIZE) + (z * CHUNK_SIZE_SQR);
+    if (voxels[index].data.y == 0) { // AIR
         return;
     }
 
-    uint vertex_offset = voxels[index].data.x * 36;
-
-    vec3 chunk_world_pos = vec3(position) * float(CHUNK_SIZE);
-    vec3 cube_pos = vec3(gl_GlobalInvocationID) - vec3(CHUNK_SIZE) * 0.5 + vec3(0.5) + chunk_world_pos;
+    const vec3 chunk_world_pos = vec3(position) * float(CHUNK_SIZE);
+    const vec3 cube_pos = vec3(gl_GlobalInvocationID) - vec3(CHUNK_SIZE) * 0.5 + vec3(0.5) + chunk_world_pos;
 
     for (uint face = 0; face < 6; face++) {
         for (uint tri_vertex = 0; tri_vertex < 6; tri_vertex++) {
-            uint index_in_face = face_indices[face][tri_vertex];
-            uint local_idx = index_in_face % 4;
+            const uint index_in_face = face_indices[face][tri_vertex];
+            const uint local_idx = index_in_face % 4;
 
             vertex_t v;
             v.position = positions[face][local_idx] + cube_pos;
@@ -56,7 +53,8 @@ void main() {
             v.uv_y = uvs[local_idx].y;
             v.color = colors[face];
 
-            uint vtx_id = vertex_offset + face * 6 + tri_vertex;
+            const uint vertex_offset = voxels[index].data.x * 36;
+            const uint vtx_id = vertex_offset + face * 6 + tri_vertex;
             vertices[vtx_id] = v;
             indices[vtx_id] = vtx_id; // No reuse
         }
