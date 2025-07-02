882c68484e95fd88dda5991e65fda2a9b2583182
Dan Liebault <d.liebault@systancia.com>
2025-07-02 12:31:11 +0200

    feat: enhance TaskManager to manage command buffers and improve task processing

--- src/engine/vulkan/queues.zig
+++ src/engine/vulkan/queues.zig
@@ -1,14 +1,18 @@
 pub const SubmitQueue = struct {
     queue: c.VkQueue,
     command_buffer_pool: c.VkCommandPool,
+    
+    available_command_buffers: std.fifo.LinearFifo(c.VkCommandBuffer, .Dynamic),
+    
     command_buffer: c.VkCommandBuffer,
     fence: c.VkFence,
     device: c.VkDevice,
 
-    pub fn init(device: c.VkDevice, queue: c.VkQueue, queue_index: u32) SubmitQueue {
+    pub fn init(allocator: std.mem.Allocator, device: c.VkDevice, queue: c.VkQueue, queue_index: u32) SubmitQueue {
         var instance: SubmitQueue = .{
             .queue = queue,
             .command_buffer_pool = undefined,
+            .available_command_buffers = std.fifo.LinearFifo(c.VkCommandBuffer, .Dynamic).init(allocator),
             .command_buffer = undefined,
             .fence = undefined,
             .device = device
@@ -24,15 +28,31 @@ pub const SubmitQueue = struct {
     pub fn deinit(self: *SubmitQueue, device: c.VkDevice) void {
         c.vkDestroyCommandPool(device, self.command_buffer_pool, null);
         c.vkDestroyFence(device, self.fence, null);
+
+        self.available_command_buffers.deinit();
     }
 
-    pub fn start_command(self: *SubmitQueue) void {
-        var result = c.vkResetFences(self.device, 1, &self.fence);
-        if (result != c.VK_SUCCESS) {
-            std.log.warn("vkResetFences failed with error {d}", .{ result });
+    pub fn expand(self: *SubmitQueue, size: usize) !void {
+        for (0..size) |_| {
+            const cmd = try commands.create_command_buffer(1, self.device, self.command_buffer_pool);
+            try self.available_command_buffers.writeItem(cmd);
         }
+    }
 
-        result = c.vkResetCommandBuffer(self.command_buffer, 0);
+    /// Get the next available VkCommandBuffer for recording
+    /// Caller is responsible for destroying the Vk objects until submit
+    pub fn next_command_buffer(self: *SubmitQueue) !c.VkCommandBuffer {
+        if (self.available_command_buffers.readItem()) |cmd| {
+            return cmd;
+        }
+        else {
+            const cmd = try commands.create_command_buffer(1, self.device, self.command_buffer_pool);
+            return cmd;
+        }
+    }
+
+    pub fn start_command(_: *SubmitQueue, cmd: c.VkCommandBuffer) void {
+        var result = c.vkResetCommandBuffer(cmd, 0);
         if (result != c.VK_SUCCESS) {
             std.log.warn("vkResetCommandBuffer failed with error {d}", .{ result });
         }
@@ -43,22 +63,29 @@ pub const SubmitQueue = struct {
             .flags = c.VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
         };
 
-        result = c.vkBeginCommandBuffer(self.command_buffer, &begin_info);
+        result = c.vkBeginCommandBuffer(cmd, &begin_info);
         if (result != c.VK_SUCCESS) {
             std.log.warn("vkBeginCommandBuffer failed with error {d}", .{ result });
         }
     }
 
-    pub fn submit_command(self: *SubmitQueue) void {
-        var result = c.vkEndCommandBuffer(self.command_buffer);
+    pub fn end_command(_: *SubmitQueue, cmd: c.VkCommandBuffer) void {
+        const result = c.vkEndCommandBuffer(cmd);
         if (result != c.VK_SUCCESS) {
             std.log.warn("vkEndCommandBuffer failed with error {d}", .{ result });
+        }
+    }
+
+    pub fn submit_command(self: *SubmitQueue, cmd: c.VkCommandBuffer) void {
+        var result = c.vkResetFences(self.device, 1, &self.fence);
+        if (result != c.VK_SUCCESS) {
+            std.log.warn("vkResetFences failed with error {d}", .{ result });
         }
 
         const cmd_submit_info = c.VkCommandBufferSubmitInfo {
             .sType = c.VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO,
             .pNext = null,
-            .commandBuffer = self.command_buffer,
+            .commandBuffer = cmd,
             .deviceMask = 0
         };
 
@@ -77,7 +104,7 @@ pub const SubmitQueue = struct {
             .waitSemaphoreInfoCount = 0,
         };
 
-        result = c.vkQueueSubmit2(self.queue, 1, &submit_info, self.fence); // TODO : run it on other queue for multithreading
+        result = c.vkQueueSubmit2(self.queue, 1, &submit_info, self.fence);
         if (result != c.VK_SUCCESS) {
             std.log.warn("vkQueueSubmit2 failed with error {d}", .{ result });
         }
@@ -86,6 +113,11 @@ pub const SubmitQueue = struct {
         if (result != c.VK_SUCCESS) {
             std.log.warn("vkWaitForFences failed with error {d}", .{ result });
         }
+
+        self.available_command_buffers.writeItem(cmd) catch {
+            std.log.warn("Failed to set command buffer back to available queue.", .{});
+            c.vkFreeCommandBuffers(self.device, self.command_buffer_pool, 1, &cmd);
+        };
     }
 };
 
--- src/tasks.zig
+++ src/tasks.zig
@@ -2,54 +2,91 @@ pub const TaskFn = *const fn(ctx: *anyopaque, cmd: c.VkCommandBuffer) void;
 pub const OnFinishTaskFn = *const fn(ctx: *anyopaque) void;
 
 pub const Task = struct {
-    func: TaskFn,
+    record: TaskFn,
     on_finish: ?OnFinishTaskFn,
     ctx: *anyopaque,
+    cmd: c.VkCommandBuffer
 };
 
 pub const TaskQueue = std.fifo.LinearFifo(Task, .Dynamic);
 
 pub const TaskManager = struct {
     allocator: std.mem.Allocator,
-    queue: TaskQueue,
-    thread: ?std.Thread = null,
+    
+    record_queue: TaskQueue,
+    submit_queue: TaskQueue,
+
+    record_thread: ?std.Thread = null,
+    submit_thread: ?std.Thread = null,
     running: bool = false,
     submit: queues.SubmitQueue,
 
     pub fn init(allocator: std.mem.Allocator, device: c.VkDevice, queue: c.VkQueue, queue_index: u32) *TaskManager {
         const tm: *TaskManager = allocator.create(TaskManager) catch {
+            std.log.err("Failed to create TaskManager !", .{});
             @panic("Out of memory !");
         };
 
         tm.* = .{
             .allocator = allocator,
-            .queue = TaskQueue.init(allocator),
-            .thread = null,
+            .record_queue = TaskQueue.init(allocator),
+            .submit_queue = TaskQueue.init(allocator),
             .running = false,
-            .submit = queues.SubmitQueue.init(device, queue, queue_index)
+            .submit = queues.SubmitQueue.init(allocator, device, queue, queue_index)
         };
         return tm;
     }
 
     pub fn deinit(self: *TaskManager) void {
-        self.running = false;
-        if (self.thread) |t| t.join();
-        self.queue.deinit();
+        self.stop();
+
+        self.record_queue.deinit();
+        self.submit_queue.deinit();
+
         self.submit.deinit(self.submit.device);
+
         self.allocator.destroy(self); // TODO : handle memory outside ?
     }
 
     pub fn enqueue(self: *TaskManager, func: TaskFn, on_finish: ?OnFinishTaskFn, ctx: *anyopaque) !void {
-        const task: Task = .{ .func = func, .on_finish = on_finish, .ctx = ctx };
-        try self.queue.writeItem(task);
+        const cmd = self.submit.next_command_buffer() catch |err| {
+            std.log.warn("Failed to get a command buffer for recording", .{});
+            return err;
+        };
+        
+        const task: Task = .{ 
+            .record = func,
+            .on_finish = on_finish,
+            .ctx = ctx,
+            .cmd = cmd,
+        };
+        try self.record_queue.writeItem(task);
     }
 
-    fn worker(self: *TaskManager) void {
+    /// Record a command buffer, then set it to the submit queue
+    fn recording_worker(self: *TaskManager) void {
         while (self.running) {
-            if (self.queue.readItem()) |task| { // TODO : batch task call, make a list of called task, and call their on finish
-                self.submit.start_command();
-                task.func(task.ctx, self.submit.command_buffer);
-                self.submit.submit_command();
+            if (self.record_queue.readItem()) |task| {
+                self.submit.start_command(task.cmd);
+                task.record(task.ctx, task.cmd);
+                self.submit.end_command(task.cmd);
+
+                // set it to the submit queue
+                self.submit_queue.writeItem(task) catch { // TODO : handle failed submission better ?
+                    std.log.err("Failed to submit command buffer", .{});
+                    @panic("Out of memory !");
+                };
+            }
+            else {
+                std.time.sleep(1_000_000); // 1ms
+            }
+        }
+    }
+
+    fn submitting_worker(self: *TaskManager) void {
+        while (self.running) {
+            if (self.submit_queue.readItem()) |task| {
+                self.submit.submit_command(task.cmd);
 
                 if (task.on_finish) |on_finish| {
                     on_finish(task.ctx);
@@ -64,13 +101,19 @@ pub const TaskManager = struct {
     pub fn start(self: *TaskManager) !void {
         if (self.running) return;
         self.running = true;
-        self.thread = try std.Thread.spawn(.{}, TaskManager.worker, .{self});
+        self.submit_thread = try std.Thread.spawn(.{}, TaskManager.submitting_worker, .{ self });
+        self.record_thread = try std.Thread.spawn(.{}, TaskManager.recording_worker, .{ self });
     }
 
+    /// Stops all the threads
     pub fn stop(self: *TaskManager) void {
         self.running = false;
-        if (self.thread) |t| t.join();
-        self.thread = null;
+
+        if (self.submit_thread) |t| t.join();
+        self.submit_thread = null;
+        
+        if (self.record_thread) |t| t.join();
+        self.record_thread = null;
     }
 };
 
