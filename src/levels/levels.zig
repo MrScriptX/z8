2b531ba4e379ede2bcc10555f706c4df1555fb72
Dan Liebault <d.liebault@systancia.com>
2025-07-11 15:00:15 +0200

    move voxels.zig to voxel/scene.zig

--- src/levels/levels.zig
+++ src/levels/levels.zig
@@ -1,3 +1,3 @@
 pub const MonkeyScene = @import("monkey.zig").MonkeyScene;
 pub const ReactorScene = @import("reactor.zig").ReactorScene;
-pub const VoxelsScene = @import("voxels.zig").VoxelScene;
+pub const VoxelsScene = @import("voxel/scene.zig").VoxelScene;
